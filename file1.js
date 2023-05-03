e33e3e7b4e4d0ef26749c92df0a218005c0cbe72
[shubhamsingh74] <shubhamsingh74970@gmail.com>
2023-05-04 02:50:27 +0530

    variables

---
+++ file1.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log('shubham singh');
+//shubham singh
+// console.log('shubham singh shuabhsm singh');
+
+var data = 21;
+console.log(data);
+
+var name = "shubham ";
+const lastname = "rajput";
+console.log(lastname+name);
+console.log(name);
