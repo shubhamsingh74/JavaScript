30b4f13c1e9d18e4a1846936fd72555b6ee4b533
[shubhamsingh74] <shubhamsingh74970@gmail.com>
2023-05-07 10:41:25 +0530

    functions

--- part1/45.js
+++ part1/45.js
@@ -3,36 +3,42 @@
 //     console.log("happy birthday to you ......");
 // }
 
-const singHappyBirthday = function(){
-    console.log("happy birthday to you ......");
-}
+// const singHappyBirthday = function(){
+//     console.log("happy birthday to you ......");
+// }
+// singHappyBirthday(); // singHappyBirthday();
 
-// singHappyBirthday();
-
-const sumThreeNumbers = function(number1, number2, number3){
-    return number1 + number2 + number3;
-}
-const ans = sumThreeNumbers(2,3,4);
+// const sumThreeNumbers = function(number1, number2, number3){
+//     return number1 + number2 + number3;
+// }
+// const ans = sumThreeNumbers(2,3,4);
 // console.log(ans);
+// console.log(sumThreeNumbers(2,3,2));
+
 
 
 // function isEven(number){
 //     return number % 2 === 0;
 // }
-const isEven = function(number){
-    return number % 2 === 0;
-}
+// const isEven = function(number){
+//     return number % 2 === 0;
+// }
 // console.log(isEven(2));
 
-const firstChar = function(anyString){
-    return anyString[0];
-}
 
-const findTarget = function(array, target){
-    for(let i = 0; i<array.length; i++){
-        if(array[i]===target){
-            return i;
-        }
-    }
-    return -1;
-}
+
+
+// const firstChar = function(anyString){
+//     return anyString[0];
+// }
+
+
+
+// const findTarget = function(array, target){
+//     for(let i = 0; i<array.length; i++){
+//         if(array[i]===target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
--- part1/47.js
+++ part1/47.js
@@ -1,10 +1,16 @@
-// hoisting 
+// hoisting
+//the meaning of the hoisting is the calling function name before the declaration.
+// its only used in the function-declaration.
+//it is not done in the arrow function and function expression.
+
 
 // hello();
 
 // function hello(){
 //     console.log("hello world");
 // }
+
+
 // console.log(hello);
 // const hello = "hello world";
 // console.log(hello);
